c959c39b7c2e24d23d519d34989e8bb6b7bb6931
Niche86 <kleandroo15@gmail.com>
2026-05-17 17:30:42 -0500

    Agrega list_display_links para editar desde el admin

--- testmichi/admin.py
+++ testmichi/admin.py
@@ -5,14 +5,17 @@ from .models import Gato, Pregunta, Respuesta
 @admin.register(Gato)
 class GatoAdmin(admin.ModelAdmin):
     list_display = ('id', 'nombre', 'descripcion', 'imagen')
+    list_display_links = ('nombre',)
 
 
 @admin.register(Pregunta)
 class PreguntaAdmin(admin.ModelAdmin):
     list_display = ('id', 'texto', 'orden')
+    list_display_links = ('texto',)
 
 
 @admin.register(Respuesta)
 class RespuestaAdmin(admin.ModelAdmin):
     list_display = ('id', 'texto', 'pregunta', 'gato')
+    list_display_links = ('texto',)
     list_filter = ('gato', 'pregunta')
